9c0daa9248f19fde6167b1a55628e1e857b86d70
Dipa1805 <q03500b@upla.edu.pe>
2024-10-04 01:02:27 -0500

    Add files via upload

---
+++ Semana03/src/main/java/com/mycompany/semana03/Semana03.java
@@ -0,0 +1,15 @@
+package com.mycompany.semana03;
+
+import Vista.CIngreso;
+/**
+ *
+ * @author diego
+ */
+public class Semana03 {
+
+    public static void main(String[] args) {
+        CIngreso vistaIngreso = new CIngreso();
+        vistaIngreso.setVisible(true);
+        vistaIngreso.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+    }
+}
