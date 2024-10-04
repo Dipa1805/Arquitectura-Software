5ec71a0af106f04b290dde825a9a719463a22d8c
Dipa1805 <q03500b@upla.edu.pe>
2024-10-04 00:49:26 -0500

    Add files via upload

---
+++ Semana1/src/main/java/com/mycompany/registroalum/RegistroAlum.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registroalum;
+
+/**
+ *
+ * @author miguel
+ */
+public class RegistroAlum {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
